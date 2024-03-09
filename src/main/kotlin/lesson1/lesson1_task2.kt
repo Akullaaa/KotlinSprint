a1637fa016ccc119ee91cd97d9637d17389103e5
Andrei <ndr.vldrsk@gmail.com>
2024-03-09 13:10:13 +0200

    KS-1-1 print data from first lesson and second task

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson1
+
+fun main() {
+
+    var numberOfOrder:Int = 75
+    var thankYouText: String = "Thank you"
+    var workers:Int = 2000
+
+    println(numberOfOrder)
+    println(thankYouText)
+
+//    println(workers)
+
+    workers--
+    println(workers)
+}
