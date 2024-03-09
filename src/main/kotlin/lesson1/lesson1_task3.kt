503f88b5bf17de525dd6adcbccf8ce719d82819b
Andrei <ndr.vldrsk@gmail.com>
2024-03-09 16:51:16 +0200

    Давай объявим строки и сделаем правильный формат)

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -2,18 +2,16 @@ package org.example.lesson1
 
 fun main() {
 
-    val GagarinFlyYear = 1961
-    var takeoffHour = 9
-    var takeoffMinute = 7
+    val GagarinFlyYear:String = "1961"
+    var takeoffHour:String = "09"
+    var takeoffMinute:String = "07"
 
     println(GagarinFlyYear)
     println(takeoffHour)
     println(takeoffMinute)
 
-    takeoffHour = 10
-    takeoffMinute = 55
+    takeoffHour = "10"
+    takeoffMinute = "55"
 
-    print(takeoffHour)
-    print(":")
-    print(takeoffMinute)
+    println("$takeoffHour:$takeoffMinute")
 }
