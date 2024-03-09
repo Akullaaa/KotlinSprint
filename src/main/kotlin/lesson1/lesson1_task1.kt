f3007e825a0f5c540793973bd0f3bb98cf00386e
Andrei <ndr.vldrsk@gmail.com>
2024-03-09 12:35:41 +0200

    KS-1-1 print data from first lesson and first task

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -4,4 +4,11 @@ fun main() {
 
     println("This is issue for task 1 from lesson 1")
 
+    var numberOfOrder:Int = 75
+
+    var thankYouText: String = "Thank you"
+
+    println(numberOfOrder)
+
+    println(thankYouText)
 }
