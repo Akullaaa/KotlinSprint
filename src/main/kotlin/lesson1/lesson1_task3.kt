fbcc41951c14f29be1542a9f6668ab79d36b2538
Andrei <ndr.vldrsk@gmail.com>
2024-03-09 13:25:40 +0200

    KS-1-3 print data from first lesson and third task

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+fun main() {
+
+    val GagarinFlyYear = 1961
+    var takeoffHour = 9
+    var takeoffMinute = 7
+
+    println(GagarinFlyYear)
+    println(takeoffHour)
+    println(takeoffMinute)
+
+    takeoffHour = 10
+    takeoffMinute = 55
+
+    println("$takeoffHour:$takeoffMinute")
+}
