2d5a0d677b851b2f6822d48012a41cdff1be682b
Andrei <ndr.vldrsk@gmail.com>
2024-03-09 18:50:41 +0200

    Задача 5* к Уроку 1
    Объяви переменную с количеством секунд, которые Гагарин провел в космосе.
    
    - Переведи в коде количество секунд в целые минуты и целые часы, сохраняя остаток секунд и минут в новые переменные;
    - Выведи в консоль время, проведенное в космосе в формате [часы:минуты:секунды], например так: 01:30:09;
    - Значения часов, минут и секунд отображать двумя цифрами.

---
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -0,0 +1,12 @@
+package org.example.lesson1
+
+fun main() {
+    val totalSeconds = 6480
+
+    val hours = totalSeconds / 3600
+    val minutes = totalSeconds % 3600 / 60
+    val seconds = totalSeconds % 60
+
+    println("%02d:%02d:%02d".format(hours,minutes,seconds))
+
+}
