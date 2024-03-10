86521438cc5eebeb51e4ff0a558af06678fc578f
Andrei <ndr.vldrsk@gmail.com>
2024-03-10 17:03:41 +0200

    все “голые” числа вынести в константу.

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,12 +1,14 @@
 package org.example.lesson1
 
 fun main() {
-    val totalSeconds = 6480
 
-    val hours = totalSeconds / 3600
-    val minutes = totalSeconds % 3600 / 60
-    val seconds = totalSeconds % 60
+	val totalSeconds = 6480
+	val secondsPerMinute = 60
+	val minutePerHour = 60
+	val hours = totalSeconds / (secondsPerMinute * minutePerHour)
+	val minutes = totalSeconds % (secondsPerMinute * minutePerHour) / secondsPerMinute
+	val seconds = totalSeconds % secondsPerMinute
 
-    println("%02d:%02d:%02d".format(hours,minutes,seconds))
+	println("%02d:%02d:%02d".format(hours,minutes,seconds))
 
 }
