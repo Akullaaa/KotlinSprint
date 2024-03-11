fcec1314621573a8f1380b7d1e5f7ae2b9c598a9
Andrei <ndr.vldrsk@gmail.com>
2024-03-11 22:36:23 +0200

    Задача 1 к Уроку 3

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,17 @@
+package org.example.lesson3
+
+fun main() {
+
+	val userName = "Груу"
+	var greetings:String
+
+	val greetingsForDay = "Добрый день, $userName"
+	val greetingsForEvening = "Добрый вечер, $userName"
+
+	greetings = greetingsForDay
+	println(greetings)
+
+	greetings = greetingsForEvening
+	println(greetings)
+
+}
