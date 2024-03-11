7c75d14dd095e2283e8bfd7697d0fbb817a41480
Andrei <ndr.vldrsk@gmail.com>
2024-03-11 22:13:20 +0200

    Для красоты лучше разбить код на блоки, разделяемые пустыми строками

--- src/main/kotlin/lesson2/lesson2_task2.kt
+++ src/main/kotlin/lesson2/lesson2_task2.kt
@@ -3,15 +3,12 @@ package org.example.lesson2
 fun main() {
 	val workers = 50
 	val workersSalary = 30000
-
 	val interns = 30
 	val internsSalary = 20000
 
 	val emploeeSalaryExpenses = workers * workersSalary
 	val internSalaryExpenses = interns * internsSalary
-
 	val totalSalaryExpenses = emploeeSalaryExpenses + internSalaryExpenses
-
 	val averageSalary = totalSalaryExpenses / (workers + interns)
 
 	println(emploeeSalaryExpenses)
