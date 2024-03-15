a1cacf92df22a785fab7362baabf799e5587708c
andrei <ndr.vldrsk@gmail.com>
2024-03-15 22:50:26 +0200

    Задача 4 к Уроку 3

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson3
+
+fun main() {
+	var fromPosition = "E2"
+	var toPositon = "E4"
+	var strokeNumber = 1
+
+//  println("[откуда-куда;номер хода]")
+	println("[$fromPosition-$toPositon;$strokeNumber]")
+
+	fromPosition = "D3"
+    toPositon = "D4"
+    strokeNumber = 3
+
+    println("[$fromPosition-$toPositon;$strokeNumber]")
+
+}
